9301e262c4fd66ce818e0685dad08614167104af
Allan Giovanni Matias Paes <109719778+Matiaszz@users.noreply.github.com>
2024-06-05 01:01:04 -0300

    exercicio-args.py

--- exercicio-args.py
+++ exercicio-args.py
@@ -8,22 +8,18 @@ def multiplicacao (*args):
     for valor in args:
         acumulador *= valor
     return(acumulador)
-    
+
 numeros = 1,2,3,4,5,6
 total = multiplicacao(*numeros)
 print(total)
 
-
 # Crie uma função fala se um número é par ou ímpar.
 # Retorne se o número é par ou ímpar
 
-def par_ou_impar(*args):
-
-    for n in args:
-        if n % 2 == 0:
-            return f'o numero {n} é par'
-        return f'o numero {n} é impar'
-
-
-var = par_ou_impar(3)
+def par_ou_impar(numero):
+        if numero % 2 == 0:
+            return f'o numero {numero} é par'
+        return f'o numero {numero} é impar'
+    
+var = par_ou_impar(6)
 print(var)
